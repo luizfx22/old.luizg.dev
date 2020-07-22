45a7f8fe99ca5ad1306f62559ae555dd63269584
Luiz <luizgomes749@gmail.com>
2020-07-22 00:43:01 -0300

    Implementing article editting and VUE

--- luizg/blog/urls.py
+++ luizg/blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.Blog.as_view(), name="blog"),
-    path('post/<slug:slug>/', views.BlogPost.as_view(), name="blog_post"),
+    path('post/<slug:slug>', views.BlogPost.as_view(), name="blog_post"),
+    path('new/post', views.NewBlogPost.as_view(), name="new_blog_post"),
 ]
--- luizg/blog/views.py
+++ luizg/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from .models import Post
 
 # Create your views here.
@@ -13,3 +14,9 @@ class BlogPost(DetailView):
   model = Post
   template_name = 'blog/post.html'
   context_object_name = 'post'
+
+
+class NewBlogPost(CreateView):
+  model = Post
+  template_name = 'blog/editor/article.html'
+  fields = ['title', 'content', 'published']
