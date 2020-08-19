b92807ae068f091b6c98f17a032e3413b294c2bf
Luiz <luizgomes749@gmail.com>
2020-08-18 22:23:48 -0300

    Use MySQL instead of sqlite and some fixes

--- luizg/luizg/settings.py
+++ luizg/luizg/settings.py
@@ -77,9 +77,17 @@ WSGI_APPLICATION = 'luizg.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'luizg',
+        'USER': 'op',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
