952cdfe6dfed0dc8142eec3ec57d809aa7e52c00
Luiz <luizgomes749@gmail.com>
2020-09-01 21:04:23 -0300

    Reindex

--- luizg/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.Blog.as_view(), name="blog"),
-    path('posts/post/<slug:slug>', views.BlogPost.as_view(), name="blog_post"),
-    path('posts/new', views.NewBlogPost.as_view(), name="new_blog_post"),
-]
--- readme.md
+++
@@ -1,9 +0,0 @@
-# luizg.dev - Source
-My Django powered personal website and blog
-
-## Not published yet... Still in DEV
-
-## Requirements:
-- [x] Django
-- [x] MySQL Connector
-- [x] PipEnv
